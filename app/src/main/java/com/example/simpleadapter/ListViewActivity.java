d1277e9404f571b4ccdfe422f2918843cf1adecb
AndreyOvch <amo-rzn@ya.ru>
2020-07-22 00:27:03 +0300

    swipeRefresh

--- app/src/main/java/com/example/simpleadapter/ListViewActivity.java
+++ app/src/main/java/com/example/simpleadapter/ListViewActivity.java
@@ -1,15 +1,20 @@
 package com.example.simpleadapter;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,8 +25,12 @@ public class ListViewActivity extends AppCompatActivity {
 
     private static final String KEY1="key1";
     private static final String KEY2="key2";
-    private ListAdapter adapter;
+    private SimpleAdapter adapter;
     private String[] values;
+    private SharedPreferences myListSharedPref;
+    private static String LIST_TEXT = "list_text";
+    private String noteTxt;
+    private List<Map<String,String>>result=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,34 +38,63 @@ public class ListViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_view);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        ListView list = findViewById(R.id.list);
-
+        myListSharedPref = getSharedPreferences("MyList",MODE_PRIVATE);
+        saveString();
         values = prepareContent();
-
+        final ListView list = findViewById(R.id.list);
+        data();
         adapter=createAdapter();
-
         list.setAdapter(adapter);
+
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+                result.remove(position);
+                adapter.notifyDataSetChanged();
+
+            }
+
+        });
+
+        final SwipeRefreshLayout refreshLayout=findViewById(R.id.swipe_refresh_layout);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                result.clear();
+                data();
+                adapter.notifyDataSetChanged();
+                refreshLayout.setRefreshing(false);
+            }
+        });
+
     }
 
+    private void saveString(){
+        SharedPreferences.Editor myEditor = myListSharedPref.edit();
+        //noteTxt = myListSharedPref.getString(LIST_TEXT, "");
+        //if(noteTxt.equals("")){
+        myEditor.putString(LIST_TEXT, getString(R.string.large_text));
+        myEditor.apply();
+        Toast.makeText(ListViewActivity.this, "Данные строки сохранены", Toast.LENGTH_LONG).show();
+        //}
+    }
 
     private String[] prepareContent() {
-
-        return getString(R.string.large_text).split("\n\n");
+        noteTxt = myListSharedPref.getString(LIST_TEXT, "");
+        //return getString(R.string.large_text).split("\n\n");
+        return noteTxt.split("\n\n");
     }
 
-    private static List<Map<String, String>> data (String KEY1, String KEY2, String[] values){
-        List<Map<String,String>>result=new ArrayList<>();
+    private void data (){
         for (int i=0; i<values.length;i++){
             Map<String,String>map=new HashMap<>();
             map.put(KEY1,values[i]);
             map.put(KEY2,values[i].length()+"");
             result.add(map);
         }
-        return result;
+
     }
-    private ListAdapter createAdapter(){
-        List<Map<String,String>>mapData=data(KEY1,KEY2,values);
-        return new SimpleAdapter(this,mapData,R.layout.list_item,new String[]{KEY1,KEY2},new int[]{R.id.text1,R.id.text2});
+    private SimpleAdapter createAdapter(){
+        return new SimpleAdapter(this,result,R.layout.list_item,new String[]{KEY1,KEY2},new int[]{R.id.text1,R.id.text2});
     }
 }
